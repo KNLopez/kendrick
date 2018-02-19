d8e4087c7b9a2d699052721ff6d84ad5476e91ae
KNLopez <kurt@kendrickinc.com>
2018-02-19 22:16:03 +0800

    contact page

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 (function(){
   var slideIndex = 0;
-  showSlides();
+  // showSlides();
 
 
   function showSlides() {
     var i;
     var slides = document.getElementsByClassName("testimonial");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    if(slides){
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      slides[slideIndex-1].style.display = "flex";
+      setTimeout(showSlides, 3000)
+    }else{
+      
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "flex";
-    setTimeout(showSlides, 3000)
   }
-  document.querySelector('.search-bar').onkeyup = function(){
-    this.value != '' ? document.querySelector('.search-results').style .display = 'block' : document.querySelector('.search-results').style .display = 'none'; 
-  }
+  var searchbar = document.querySelector('.search-bar');
+  if(searchbar) searchbar.onkeyup = function(){    this.value != '' ? document.querySelector('.search-results').style .display = 'block' : document.querySelector('.search-results').style .display = 'none';   }
   document.querySelector("#nav-toggle")
   .addEventListener("click", function () {
     this.classList.toggle("active");
